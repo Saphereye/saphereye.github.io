b488cbb4fecea31bae7a134902a2e52503d6274e
Saphereye <adarshdas950@gmail.com>
2024-02-04 13:13:19 +0530

    style: :art: Changed code for map display. Shouldn't look tiny on mobile

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-                <div className="lg:w-1/2 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                <div className="flex-grow bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative min-h-[400px]">
                     <iframe
                         width="100%"
                         height="100%"
@@ -14,7 +14,7 @@ export default function Contact() {
                         src="https://www.google.com/maps/embed/v1/place?q=bits-hyderabad&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
                     />
                 </div>
-                <div className="flex items-center min-h-[400px] flex-col flex-grow">
+                <div className="flex items-center flex-col p-2">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                         Contact Me
                     </h2>
