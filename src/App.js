482419f7ca19934777578d0aa406d19101ff996d
Saphereye <adarshdas950@gmail.com>
2024-02-11 21:56:50 +0530

    feat: :lipstick: Added waves at end of contacts

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export function useIsVisible(ref) {
   useEffect(() => {
     const observer = new IntersectionObserver(([entry]) => {
       setIntersecting(entry.isIntersecting)
-    },{
+    }, {
       threshold: 0.1
     }
     );
@@ -64,6 +64,24 @@ const Home = () => {
       </div>
       <div ref={contactRef} className={`transition-opacity ease-in duration-700 ${isVisibleContact ? "opacity-100" : "opacity-0"}`}>
         <Contact />
+      </div>
+      <div>
+        <div class="relative w-full bottom-0">
+          <svg class="w-full h-40 mb-8" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 40" preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+              <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="moving-waves">
+              <use xlinkHref="#gentle-wave" x="48" y="-1" fill="rgba(255,255,255,0.40)" />
+              <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.35)" />
+              <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.25)" />
+              <use xlinkHref="#gentle-wave" x="48" y="8" fill="rgba(255,255,255,0.20)" />
+              <use xlinkHref="#gentle-wave" x="48" y="13" fill="rgba(255,255,255,0.15)" />
+              <use xlinkHref="#gentle-wave" x="48" y="16" fill="rgba(255,255,255,1)" />
+            </g>
+          </svg>
+        </div>
       </div>
     </>
   );
